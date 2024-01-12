0bbde82915b9d532c8fafad48c0501f82c89f5ff
brsieg99 <93331099+brsieg99@users.noreply.github.com>
2024-01-12 16:49:55 +0100

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const cors = require('cors');
 const { Client } = require('pg');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000; // Port anpassen
 
 app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
